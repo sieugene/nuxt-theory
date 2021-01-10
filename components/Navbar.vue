ca6f5050a825b93958b23c5553d708bdd3c9f67c
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-10 19:32:28 +0300

    error page, active link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,15 +6,28 @@
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <nuxt-link class="nav-link active" aria-current="page" to="/"
+              <nuxt-link
+                exact
+                active-class="active"
+                class="nav-link"
+                aria-current="page"
+                to="/"
                 >Home</nuxt-link
               >
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/users">Users</nuxt-link>
+              <nuxt-link
+                class="nav-link"
+                no-prefetch
+                active-class="active"
+                to="/users"
+                >Users</nuxt-link
+              >
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+              <nuxt-link class="nav-link" active-class="active" to="/about"
+                >About</nuxt-link
+              >
             </li>
           </ul>
         </div>
