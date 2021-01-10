31d3db5aba2090af0f6ec8dd71d2ebf61d515d9f
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-10 18:51:54 +0300

    basic

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Users</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
