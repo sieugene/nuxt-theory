d633304b22120c1207bd22afd6aa0c339b5cb34f
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-10 19:56:19 +0300

    add axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,9 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: ['@nuxtjs/axios'],
+
+  axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
