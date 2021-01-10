7254854e32487f97a9891f5497a99fb044f8d80c
sieugene <evgenii.sidelnikov@unitbean.io>
2021-01-10 19:01:09 +0300

    routing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,14 +2,19 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <nuxt-link class="navbar-brand" to="/">Navbar</nuxt-link>
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <nuxt-link class="nav-link active" aria-current="page" to="/"
+                >Home</nuxt-link
+              >
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Users</a>
+              <nuxt-link class="nav-link" to="/users">Users</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/about">About</nuxt-link>
             </li>
           </ul>
         </div>
